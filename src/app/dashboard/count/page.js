08f8ca843376fe8824b36438e13bca26312138ff
Rohan sahu <sahurohankumar47@gmail.com>
2026-03-25 12:22:01 +0530

    modified

--- src/app/dashboard/count/page.js
+++ src/app/dashboard/count/page.js
@@ -1,12 +1,19 @@
-import Count from '@/webPages/Count/Count'
-import React from 'react'
+'use client'
+import { Suspense } from 'react';
+import Count from '@/webPages/Count/Count';
 
-function page() {
+function Page() {
   return (
-    <div>
-      <Count/>
-    </div>
-  )
+    <Suspense
+      fallback={
+        <div className="flex items-center justify-center min-h-screen">
+          <div className="h-8 w-8 animate-spin rounded-full border-4 border-red-600 border-t-transparent"></div>
+        </div>
+      }
+    >
+      <Count />
+    </Suspense>
+  );
 }
 
-export default page
+export default Page;
